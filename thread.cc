77024594f6032f298767786be94101294341c7fa
Maverick Chung <mc608@duke.edu>
2020-09-28 22:28:29 -0400

    file

--- thread.cc
+++ thread.cc
@@ -2,4 +2,43 @@
 #include "thread.h"
 #include <ucontext.h>
 
-// CODE GOES HERE
+#include <stdio.h>
+#include <string.h>
+#include <deque> 
+#include <cmath>
+
+int thread_libinit(thread_startfunc_t func, void *arg){
+
+
+	cout << "Thread library exiting.\n";
+	exit(0);
+}
+
+int thread_create(thread_startfunc_t func, void *arg){
+
+}
+
+int thread_yield(void){
+
+}
+
+/*
+int thread_lock(unsigned int lock){
+
+}
+
+int thread_unlock(unsigned int lock){
+
+}
+
+int thread_wait(unsigned int lock, unsigned int cond){
+
+}
+
+int thread_signal(unsigned int lock, unsigned int cond){
+
+}
+
+int thread_broadcast(unsigned int lock, unsigned int cond){
+
+}*/
